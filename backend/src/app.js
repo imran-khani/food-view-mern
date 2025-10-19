019e1be2cb2b51903e2876ba5e3f4662a8e12a51
Imran Khan <imran@isupercoder.com>
2025-10-19 13:24:26 +0500

    feat(auth): implement user registration logic
    Add the core functionality for user registration. This commit installs the necessary dependencies for authentication and implements the controller logic.
    
    - Install bcrypt, jsonwebtoken, and cookie-parser for password hashing, token generation, and cookie handling.
    - Update the `registerUser` controller to check for existing users, hash the incoming password, and create a new user in the database.
    - Generate a JSON Web Token (JWT) upon successful user creation.
    - Add `cookie-parser` middleware to the main Express application.
    - Include a new `.editorconfig` file to enforce consistent coding styles.

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,11 @@
 import express from 'express'
 import { configDotenv } from 'dotenv'
+import cookieParser from 'cookie-parser'
 
 const app = express()
 configDotenv()
 app.use(express.json())
+app.use(cookieParser)
 
 
 
--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,12 +1,29 @@
-import {userModel} from '../models/user.model.js'
+import { userModel } from "../models/user.model.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
+const registerUser = async (req, res) => {
+    const { fullName, email, password } = req.body;
 
-const registerUser = async (req,res)=> {
-    const {fullName,email,password} = req.body
+    const userExist = await userModel.findOne({ email });
 
-    try {
-        
-    } catch (error) {
-        console.log(error)
+    if (userExist) {
+        return res.status(400).json({
+            message: "User already exist ",
+        });
     }
-}
+
+    const saltedPassword = bcrypt.hash(password, 11);
+    const user = await userModel.create({
+        fullName,
+        email,
+        password: saltedPassword,
+    });
+
+    const token = jwt.sign(
+        {
+            id: user._id,
+        },
+        process.env.jwtSecret
+    );
+};
